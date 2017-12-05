ded2c8147dbe132e2a56589d014d2b203f44920a
Thomas <thomas@lekoala.be>
2017-12-05 08:27:47 +0100

    fix http auth

--- code/ContentController.php
+++ code/ContentController.php
@@ -3,6 +3,8 @@ namespace LeKoala\Base;
 
 use \Exception;
 use SilverStripe\i18n\i18n;
+use SilverStripe\Control\Director;
+use SilverStripe\Core\Environment;
 use SilverStripe\View\Requirements;
 use LeKoala\Base\Helpers\ClassHelper;
 use SilverStripe\ORM\ValidationResult;
@@ -35,10 +37,40 @@ class ContentController extends DefaultController
 
     protected function init()
     {
+        if (Director::isTest()) {
+            $this->requireHttpBasicAuth();
+        }
+
         parent::init();
 
         $this->warnIfWrongCacheIsUsed();
         $this->displayFlashMessage();
+    }
+
+    /**
+     * A simple way to http protected a website (for staging for instance)
+     * This is required because somehow the default mechanism shipped with SilverStripe is
+     * not working properly
+     *
+     * @return void
+     */
+    protected function requireHttpBasicAuth()
+    {
+        $user = Environment::getEnv('SS_DEFAULT_ADMIN_USERNAME');
+        $password = Environment::getEnv('SS_DEFAULT_ADMIN_PASSWORD');
+
+        header('Cache-Control: no-cache, must-revalidate, max-age=0');
+        $hasSuppliedCredentials = ! (empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
+        if ($hasSuppliedCredentials) {
+            $isNotAuthenticated = ($_SERVER['PHP_AUTH_USER'] != $user || $_SERVER['PHP_AUTH_PW'] != $password);
+        } else {
+            $isNotAuthenticated = true;
+        }
+        if ($isNotAuthenticated) {
+            header('HTTP/1.1 401 Authorization Required');
+            header('WWW-Authenticate: Basic realm="Access denied"');
+            exit;
+        }
     }
 
     /**
