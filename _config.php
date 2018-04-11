89155bb22a6698fd04b01c52c9498a6e19b317bf
Thomas <thomas@lekoala.be>
2018-04-11 15:03:01 +0200

    cleanup config.php

--- _config.php
+++ _config.php
@@ -1,69 +1,27 @@
 <?php
 
 use SilverStripe\Control\Director;
-use SilverStripe\SQLite\SQLite3Database;
-use SilverStripe\SQLite\SQLite;
-use SilverStripe\ORM\Search\FulltextSearchable;
 
 if (!function_exists('bm')) {
-    /**
-     * A dead simple benchmark function
-     * Usage : bm(function() { // Insert here the code to benchmark });
-     * Alternative usage : bm() ; // Code to test ; bm();
-     */
     function bm($cb = null)
     {
-        static $data = null;
-
-        if ($cb === null) {
-            if ($data === null) {
-                $data = [
-                    'startTime' => microtime(true),
-                    'startMemory' => memory_get_usage(),
-                ];
-            // Allow another call
-                return;
-            }
-            else {
-                $startTime = $data['startTime'];
-                $startMemory = $data['startMemory'];
-            }
-        }
-        else {
-            $startTime = microtime(true);
-            $startMemory = memory_get_usage();
-
-            $cb();
-        }
-
-        $endTime = microtime(true);
-        $endMemory = memory_get_usage();
-
-        $time = sprintf("%.6f seconds", $endTime - $startTime);
-
-        $bytes = $endMemory - $startMemory;
-        $decimals = 2;
-        $sz = 'BKMGTP';
-        $factor = floor( (strlen($bytes) - 1) / 3);
-        $memory = sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
-
-        printf("It took %s seconds and used %s", $time, $memory);
-        die();
-
+        \LeKoala\Dev\Benchmark::run($cb);
     };
 }
 
-if(Director::isDev()) {
+if (Director::isDev()) {
     error_reporting(-1);
-    ini_set('display_errors',true);
+    ini_set('display_errors', true);
 }
 
 // When running tests, use SQLite3
 // @link https://docs.silverstripe.org/en/4/developer_guides/testing/
-if(Director::is_cli()) {
-    if(strpos($_SERVER['PHP_SELF'], 'phpunit/') !== false) {
+// Currently, some issue when running test, see pull request
+// @link https://github.com/silverstripe/silverstripe-sqlite3/pull/43
+if (Director::is_cli()) {
+    if (strpos($_SERVER['PHP_SELF'], 'phpunit/') !== false) {
         global $databaseConfig;
-        if(class_exists(SQLite3Database::class)) {
+        if (class_exists(\SilverStripe\SQLite\SQLite3Database::class)) {
             $databaseConfig['type'] = 'SQLite3Database';
             $databaseConfig['path'] = ':memory:';
         }
---
+++ code/Dev/Benchmark.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace LeKoala\Base\Dev;
+
+class Benchmark
+{
+
+    /**
+     * A dead simple benchmark function
+     *
+     * Usage : bm(function() { // Insert here the code to benchmark });
+     * Alternative usage : bm() ; // Code to test ; bm();
+     *
+     * @param callable $cb
+     * @return void
+     */
+    public static function run($cb = null)
+    {
+        static $data = null;
+
+        // No callback scenario
+        if ($cb === null) {
+            if ($data === null) {
+                $data = [
+                    'startTime' => microtime(true),
+                    'startMemory' => memory_get_usage(),
+                ];
+                // Allow another call
+                return;
+            } else {
+                $startTime = $data['startTime'];
+                $startMemory = $data['startMemory'];
+            }
+        } else {
+            $startTime = microtime(true);
+            $startMemory = memory_get_usage();
+
+            $cb();
+        }
+
+        $endTime = microtime(true);
+        $endMemory = memory_get_usage();
+
+        $time = sprintf("%.6f seconds", $endTime - $startTime);
+
+        $bytes = $endMemory - $startMemory;
+        $decimals = 2;
+        $sz = 'BKMGTP';
+        $factor = floor((strlen($bytes) - 1) / 3);
+        $memory = sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
+
+        printf("It took %s seconds and used %s", $time, $memory);
+        die();
+    }
+}
