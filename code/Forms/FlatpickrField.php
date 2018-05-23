c06e083abc918dd77723e21b264095ef627ffd37
Thomas <thomas@lekoala.be>
2018-05-23 11:56:07 +0200

    flatpickr upgrade

--- code/Forms/FlatpickrField.php
+++ code/Forms/FlatpickrField.php
@@ -1,22 +1,50 @@
 <?php
 namespace LeKoala\Base\Forms;
 
+use IntlDateFormatter;
 use SilverStripe\i18n\i18n;
 use SilverStripe\Forms\TextField;
 use SilverStripe\View\Requirements;
 use SilverStripe\Control\Controller;
+use SilverStripe\ORM\FieldType\DBDatetime;
 
 /**
  * @link https://chmln.github.io/flatpickr
  */
 class FlatpickrField extends TextField
 {
+    const DEFAULT_DATE_FORMAT = 'Y-m-d';
+    const DEFAULT_TIME_FORMAT = 'H:i';
+    const DEFAULT_DATETIME_FORMAT = 'Y-m-d H:i';
+    const DEFAULT_ALT_DATE_FORMAT = 'l j F Y';
+    const DEFAULT_ALT_TIME_FORMAT = 'H:i';
+    const DEFAULT_ALT_DATETIME_FORMAT = 'l j F Y H:i';
+
+    /**
+     * @var bool
+     */
+    protected $html5 = true;
+
     /**
      * Override locale. If empty will default to current locale
      *
      * @var string
      */
     protected $locale = null;
+
+    /**
+     * Override date format. If empty will default to that used by the current locale.
+     *
+     * @var null
+     */
+    protected $datetimeFormat = null;
+
+    /**
+     * Custom timezone
+     *
+     * @var string
+     */
+    protected $timezone = null;
 
     /**
      * Config array
@@ -29,7 +57,7 @@ class FlatpickrField extends TextField
      * @config
      * @var string
      */
-    private static $version = '4.4.4';
+    private static $version = '4.5.0';
 
     /**
      * @config
@@ -37,6 +65,9 @@ class FlatpickrField extends TextField
      */
     private static $default_config = [
         'altInput' => true,
+        'altInputClass' => 'flatpickr-alt',
+        'defaultDate' => '',
+        'time_24hr' => true,
     ];
 
     public function __construct($name, $title = null, $value = '', $maxLength = null, $form = null)
@@ -44,6 +75,26 @@ class FlatpickrField extends TextField
         parent::__construct($name, $title, $value, $maxLength, $form);
 
         $this->config = self::config()->default_config;
+        $this->setDatetimeFormat($this->convertDatetimeFormat(self::DEFAULT_ALT_DATE_FORMAT));
+        $this->setAltFormat(self::DEFAULT_ALT_DATE_FORMAT);
+    }
+
+    /**
+     * Convert a datetime format from Flatpickr to CLDR
+     *
+     * This allow to display the right format in php
+     *
+     * @see https://flatpickr.js.org/formatting/
+     * @param string $format
+     * @return string
+     */
+    protected function convertDatetimeFormat($format)
+    {
+        return str_replace(
+            ['F','l','j','d','H','i','s'],
+            ['MMMM','cccc','d','dd','HH','mm','ss'],
+            $format
+        );
     }
 
     public function Type()
@@ -56,13 +107,27 @@ class FlatpickrField extends TextField
         return 'text ' . parent::extraClass();
     }
 
+    /**
+     * Get a config key value
+     *
+     * @see https://flatpickr.js.org/options/
+     * @param string $key
+     * @return string
+     */
     public function getConfig($key)
     {
-        if (isset($this->config)) {
+        if (isset($this->config[$key])) {
             return $this->config[$key];
         }
     }
 
+    /**
+     * Set a config value
+     *
+     * @param string $key
+     * @param string $value
+     * @return string
+     */
     public function setConfig($key, $value)
     {
         if ($value) {
@@ -80,9 +145,28 @@ class FlatpickrField extends TextField
 
     public function setEnableTime($value)
     {
+        $this->setDatetimeFormat($this->convertDatetimeFormat(self::DEFAULT_ALT_DATETIME_FORMAT));
+        $this->setAltFormat(self::DEFAULT_ALT_DATETIME_FORMAT);
         return $this->setConfig('enableTime', $value);
     }
 
+    public function getNoCalendar()
+    {
+        return $this->getConfig('noCalendar');
+    }
+
+    public function setNoCalendar($value)
+    {
+        $this->setDatetimeFormat($this->convertDatetimeFormat(self::DEFAULT_ALT_TIME_FORMAT));
+        $this->setAltFormat(self::DEFAULT_ALT_TIME_FORMAT);
+        return $this->setConfig('noCalendar', $value);
+    }
+
+    /**
+     * Show the user a readable date (as per altFormat), but return something totally different to the server.
+     *
+     * @return string
+     */
     public function getAltInput()
     {
         return $this->getConfig('altInput');
@@ -91,6 +175,21 @@ class FlatpickrField extends TextField
     public function setAltInput($value)
     {
         return $this->setConfig('altInput', $value);
+    }
+
+    /**
+     * Exactly the same as date format, but for the altInput field
+     *
+     * @return string
+     */
+    public function getAltFormat()
+    {
+        return $this->getConfig('altFormat');
+    }
+
+    public function setAltFormat($value)
+    {
+        return $this->setConfig('altFormat', $value);
     }
 
     public function getMinDate()
@@ -123,6 +222,16 @@ class FlatpickrField extends TextField
         return $this->setConfig('defaultDate', $value);
     }
 
+    public function getDateFormat()
+    {
+        return $this->getConfig('dateFormat');
+    }
+
+    public function setDateFormat($value)
+    {
+        return $this->setConfig('dateFormat', $value);
+    }
+
     /**
      * Get locale to use for this field
      *
@@ -148,6 +257,15 @@ class FlatpickrField extends TextField
     }
 
     /**
+     * @return bool
+     */
+    public function getHTML5()
+    {
+        return $this->html5;
+    }
+
+
+    /**
      * This is required (and ignored) because DBDate use this to scaffold the field
      *
      * @param boolean $bool
@@ -155,7 +273,90 @@ class FlatpickrField extends TextField
      */
     public function setHTML5($bool)
     {
+        $this->html5 = $bool;
         return $this;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getTimezone()
+    {
+        return $this->timezone;
+    }
+
+    /**
+     * @param string $timezone
+     * @return $this
+     */
+    public function setTimezone($timezone)
+    {
+        if ($this->value && $timezone !== $this->timezone) {
+            throw new \BadMethodCallException("Can't change timezone after setting a value");
+        }
+
+        $this->timezone = $timezone;
+
+        return $this;
+    }
+
+    /**
+     * Get date format in CLDR standard format
+     *
+     * This can be set explicitly. If not, this will be generated from the current locale
+     * with the current date length.
+     * @see http://userguide.icu-project.org/formatparse/datetime#TOC-Date-Field-Symbol-Table
+     */
+    public function getDatetimeFormat()
+    {
+        if ($this->datetimeFormat) {
+            return $this->datetimeFormat;
+        }
+
+        // Get from locale
+        return $this->getFrontendFormatter()->getPattern();
+    }
+
+    /**
+     * Set date format in CLDR standard format.
+     * Only applicable with {@link setHTML5(false)}.
+     *
+     * @see http://userguide.icu-project.org/formatparse/datetime#TOC-Date-Field-Symbol-Table
+     * @param string $format
+     * @return $this
+     */
+    public function setDatetimeFormat($format)
+    {
+        $this->datetimeFormat = $format;
+        return $this;
+    }
+
+    /**
+    * Get date formatter with the standard locale / date format
+    *
+    * @throws \LogicException
+    * @return IntlDateFormatter
+    */
+    protected function getFrontendFormatter()
+    {
+        $formatter = IntlDateFormatter::create(
+            $this->getLocale(),
+            IntlDateFormatter::MEDIUM,
+            IntlDateFormatter::MEDIUM,
+            $this->getTimezone()
+        );
+
+        if ($this->datetimeFormat) {
+            // Don't invoke getDatetimeFormat() directly to avoid infinite loop
+            $ok = $formatter->setPattern($this->datetimeFormat);
+            if (!$ok) {
+                throw new InvalidArgumentException("Invalid date format {$this->datetimeFormat}");
+            }
+        } else {
+            $formatter->setPattern(DBDatetime::ISO_DATETIME_NORMALISED);
+        }
+        return $formatter;
     }
 
 
@@ -167,8 +368,11 @@ class FlatpickrField extends TextField
             $this->setConfig('locale', $lang);
         }
 
-        $this->setAttribute('data-flatpickr', json_encode($this->config));
+        $this->setAttribute('data-module', 'flatpickr');
+        $this->setAttribute('data-config', json_encode($this->config));
         self::requirements($lang);
+
+        $this->setAttribute('placeholder', _t('FlatpickrField.SELECT_A_DATE', 'Select a date...'));
 
         return parent::Field($properties);
     }
@@ -179,16 +383,11 @@ class FlatpickrField extends TextField
             $lang = substr(i18n::get_locale(), 0, 2);
         }
         $version = self::config()->version;
+        Requirements::javascript('base/javascript/ModularBehaviour.js', ['defer' => true]);
         Requirements::css("https://cdnjs.cloudflare.com/ajax/libs/flatpickr/$version/flatpickr.min.css");
         Requirements::javascript("https://cdnjs.cloudflare.com/ajax/libs/flatpickr/$version/flatpickr.js", ['defer' => true]);
         if ($lang != 'en') {
             Requirements::javascript("https://cdnjs.cloudflare.com/ajax/libs/flatpickr/$version/l10n/$lang.js", ['defer' => true]);
         }
-        // in frontend, we need entwine
-        $ctrl = Controller::curr();
-        if ($ctrl instanceof Page) {
-            Requirements::javascript('base/javascript/entwine/jquery.entwine-dist.js');
-        }
-        Requirements::javascript('base/javascript/fields/FlatpickrField.js', ['defer' => true]);
     }
 }
