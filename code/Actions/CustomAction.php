2f21973b8365caa0ebd3042ecc304d65b999c36c
Thomas Portelange <thomas@lekoala.Be>
2018-07-11 12:32:23 +0200

    default class for custom action

--- code/Actions/CustomAction.php
+++ code/Actions/CustomAction.php
@@ -5,12 +5,22 @@ use SilverStripe\Forms\FormAction;
 
 class CustomAction extends FormAction
 {
-
     public function __construct($name, $title, $form = null)
     {
         // Actually, an array works just fine!
         $name = 'doCustomAction[' . $name . ']';
 
         parent::__construct($name, $title, $form);
+    }
+
+    public function Field($properties = array())
+    {
+        // Add a default look if none set
+        $extra = $this->extraClass();
+        if (strpos($extra, 'btn-') === false) {
+            $this->addExtraClass('btn-info');
+        }
+
+        return parent::Field($properties);
     }
 }
