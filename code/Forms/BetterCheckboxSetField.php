66b0f061a48930ce31e410f1d73c7c963d5c10d8
Thomas <thomas@lekoala.be>
2018-07-12 12:03:40 +0200

    better support for legacy format

--- code/Forms/BetterCheckboxSetField.php
+++ code/Forms/BetterCheckboxSetField.php
@@ -10,9 +10,13 @@ use SilverStripe\Forms\CheckboxSetField;
  * $MyCheckboxField = new BetterCheckboxSetField('MyCheckboxField', null, $src);
  * $MyCheckboxField->addExtraClass('row');
  * $MyCheckboxField->addExtraItemClass('col-md-4');
+ *
+ * Support legacy encoding (comma vs json)
  */
 class BetterCheckboxSetField extends CheckboxSetField
 {
+    const LEGACY_SEPARATOR = ',';
+
     protected $extraItemClass = [];
 
     public function Type()
@@ -54,5 +58,32 @@ class BetterCheckboxSetField extends CheckboxSetField
         }
 
         return $this;
+    }
+
+    /**
+     * Extract a string value into an array of values
+     *
+     * @param string $value
+     * @return array
+     */
+    protected function stringDecode($value)
+    {
+        // Handle empty case
+        if (empty($value)) {
+            return array();
+        }
+
+        // We have a json encoded array
+        if (strpos($value, '[') === 0) {
+            $result = json_decode($value, true);
+        } else {
+            $result = explode(self::LEGACY_SEPARATOR, $value);
+        }
+
+        if ($result !== false) {
+            return $result;
+        }
+
+        throw new InvalidArgumentException("Could not decode : $value");
     }
 }
