09aff5032430ec1d8813768afb9361a35f531172
Thomas <thomas@lekoala.be>
2018-10-03 16:01:56 +0200

    DBBetterDate

---
+++ src/ORM/FieldType/DBBetterDate.php
@@ -0,0 +1,42 @@
+<?php
+namespace LeKoala\Base\ORM\FieldType;
+
+use Exception;
+use SilverStripe\ORM\FieldType\DBDate;
+
+/**
+ * This one does not crash in case your database contains rubbish data
+ */
+class DBBetterDate extends DBDate
+{
+    /**
+     * Fix non-iso dates
+     *
+     * @param string $value
+     * @return string
+     */
+    protected function fixInputDate($value)
+    {
+        try {
+            // split
+            list($year, $month, $day, $time) = $this->explodeDateString($value);
+        } catch (Exception $ex) {
+            $year = 0;
+            $month = 0;
+            $day = 0;
+        }
+
+        if ((int)$year === 0 && (int)$month === 0 && (int)$day === 0) {
+            return null;
+        }
+        // Validate date
+        if (!checkdate($month, $day, $year)) {
+            throw new InvalidArgumentException(
+                "Invalid date: '$value'. Use " . self::ISO_DATE . " to prevent this error."
+            );
+        }
+
+        // Convert to y-m-d
+        return sprintf('%d-%02d-%02d%s', $year, $month, $day, $time);
+    }
+}
