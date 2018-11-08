e88934d32c85b545adcc8f308c8cfd66f686a65d
Thomas <thomas@lekoala.be>
2018-11-08 11:25:54 +0100

    avoid duplicated theme tab

--- src/Theme/ThemeSiteConfigExtension.php
+++ src/Theme/ThemeSiteConfigExtension.php
@@ -153,6 +153,7 @@ class ThemeSiteConfigExtension extends DataExtension
     }
     public function updateCMSFields(FieldList $fields)
     {
+        $fields->removeByName('Theme');
         $themeTab = new Tab("Theme");
         $fields->addFieldToTab('Root', $themeTab);
         // If we have themes, allow to configure some css variables in them
