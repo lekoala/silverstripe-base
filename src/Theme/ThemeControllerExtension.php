b05c81da70cac6c0f691cb8e6d47c3b17bd3f955
Thomas <thomas@lekoala.be>
2019-02-18 17:16:08 +0100

    improve sample theme

--- src/Theme/ThemeControllerExtension.php
+++ src/Theme/ThemeControllerExtension.php
@@ -70,7 +70,7 @@ class ThemeControllerExtension extends Extension
      * This allows to use CSS3 variable as configurable variables in your themes
      *
      * :root {
-     * --header-font: "Roboto", serif;
+     * --header-font-family: "Roboto", serif;
      * }
      *
      * Will look for the HeaderFont property and be replaced accordingly
