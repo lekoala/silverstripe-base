dcf5cb41ffa917697b2b14196699e70ad7705499
Thomas <thomas@lekoala.be>
2019-02-21 17:46:10 +0100

    refactor n ews

--- src/News/NewsPage.php
+++ src/News/NewsPage.php
@@ -17,8 +17,7 @@ use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
 class NewsPage extends \Page
 {
     private static $table_name = 'NewsPage'; // When using namespace, specify table name
-    private static $db = [
-    ];
+    private static $db = [];
     private static $has_many = [
         "Items" => NewsItem::class,
         "Categories" => NewsCategory::class,
@@ -33,5 +32,29 @@ class NewsPage extends \Page
         $Categories = new GridField('Categories', $this->fieldLabel('Categories'), $this->Categories(), $CategoriesConfig);
         $fields->addFieldsToTab('Root.Categories', $Categories);
         return $fields;
+    }
+    /**
+     * @return DataList
+     */
+    public function DisplayedItems()
+    {
+        $list = $this->Items();
+        // Exclude unpublished and future items
+        $list = $list->where(NewsItem::defaultWhere());
+        return $list;
+    }
+    /**
+     * @return DataList
+     */
+    public function PopularItems($n = 3)
+    {
+        return $this->DisplayedItems()->sort('ViewCount DESC')->limit($n);
+    }
+    /**
+     * @return DataList
+     */
+    public function LatestItems($n = 3)
+    {
+        return $this->DisplayedItems()->sort('Published DESC')->limit($n);
     }
 }
