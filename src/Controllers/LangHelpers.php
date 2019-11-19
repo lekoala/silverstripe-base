6cfe2dccd96ee2e4bba6b59859546106c3079bd8
Thomas <thomas@lekoala.be>
2019-11-19 14:25:06 +0100

    update lang helpers

--- src/Controllers/LangHelpers.php
+++ src/Controllers/LangHelpers.php
@@ -2,6 +2,8 @@
 
 namespace LeKoala\Base\Controllers;
 
+use LeKoala\Base\i18n\BaseI18n;
+
 trait LangHelpers
 {
     /**
@@ -10,5 +12,26 @@ trait LangHelpers
     public function CurrentLang()
     {
         return substr($this->ContentLocale(), 0, 2);
+    }
+
+    /**
+     * @param string $lang
+     * @return bool
+     */
+    public function IsCurrentLang($lang)
+    {
+        return $lang == $this->CurrentLang();
+    }
+
+    /**
+     * Get the link variant for a given lang
+     *
+     * @param string $lang
+     * @return string
+     */
+    public function LangLink($lang)
+    {
+        $locale = BaseI18n::get_locale_from_lang($lang);
+        return $this->LocaleLink($locale);
     }
 }
