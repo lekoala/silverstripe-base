7bf121e4cd88b5319e88b4373d988077a912182c
Thomas <thomas@lekoala.be>
2019-11-20 10:51:45 +0100

    faq is sortable

--- src/Faq/FaqPage.php
+++ src/Faq/FaqPage.php
@@ -2,6 +2,7 @@
 
 namespace LeKoala\Base\Faq;
 
+use LeKoala\Base\Extensions\SortableExtension;
 use LeKoala\Base\Faq\FaqItem;
 use SilverStripe\Forms\TextField;
 use LeKoala\Base\Faq\FaqCategory;
@@ -9,6 +10,7 @@ use SilverStripe\Forms\LiteralField;
 use SilverStripe\SiteConfig\SiteConfig;
 use SilverStripe\Forms\GridField\GridField;
 use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
+use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
 
 /**
  * Class \LeKoala\Base\Faq\FaqPage
@@ -28,9 +30,21 @@ class FaqPage extends \Page
     {
         $fields = parent::getCMSFields();
         $ItemsConfig = GridFieldConfig_RecordEditor::create();
+
+        $singl = singleton(FaqItem::class);
+        if ($singl->hasExtension(SortableExtension::class)) {
+            $ItemsConfig->addComponent(new GridFieldOrderableRows());
+        }
+
         $Items = new GridField('Items', $this->fieldLabel('Items'), $this->Items(), $ItemsConfig);
         $fields->addFieldToTab('Root.Items', $Items);
         $CategoriesConfig = GridFieldConfig_RecordEditor::create();
+
+        $singl = singleton(FaqCategory::class);
+        if ($singl->hasExtension(SortableExtension::class)) {
+            $CategoriesConfig->addComponent(new GridFieldOrderableRows());
+        }
+
         $Categories = new GridField('Categories', $this->fieldLabel('Categories'), $this->Categories(), $CategoriesConfig);
         $fields->addFieldToTab('Root.Categories', $Categories);
         return $fields;
