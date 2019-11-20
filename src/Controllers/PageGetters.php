5f20c0551cdb23b473753b9ff8d5c45679b4b69e
Thomas <thomas@lekoala.be>
2019-11-20 10:22:34 +0100

    update page getter

--- src/Controllers/PageGetters.php
+++ src/Controllers/PageGetters.php
@@ -4,6 +4,7 @@ namespace LeKoala\Base\Controllers;
 use LeKoala\Base\News\NewsPage;
 use SilverStripe\ORM\DataObject;
 use LeKoala\Base\Contact\ContactPage;
+use LeKoala\Base\Faq\FaqPage;
 use LeKoala\Base\Privacy\PrivacyNoticePage;
 use LeKoala\Base\Privacy\CookiesRequiredPage;
 use LeKoala\Base\Privacy\TermsAndConditionsPage;
@@ -48,5 +49,13 @@ trait PageGetters
     public function NewsPage()
     {
         return DataObject::get_one(NewsPage::class);
+    }
+
+    /**
+     * @return FaqPage
+     */
+    public function FaqPage()
+    {
+        return DataObject::get_one(FaqPage::class);
     }
 }
