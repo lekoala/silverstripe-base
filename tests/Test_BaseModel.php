bf0464f250bbed064e94dda380e050e5d1913ec8
Thomas <thomas@lekoala.be>
2021-01-06 19:03:10 +0100

    move phone number stuff to external module

--- tests/Test_BaseModel.php
+++ tests/Test_BaseModel.php
@@ -5,13 +5,12 @@ namespace LeKoala\Base\Test;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\Dev\TestOnly;
 use LeKoala\Base\Extensions\IPExtension;
-use LeKoala\Base\ORM\FieldType\DBPhone;
 use LeKoala\Base\ORM\FieldType\DBCountry;
 
 class Test_BaseModel extends DataObject implements TestOnly
 {
     private static $db = [
-        "Phone" => DBPhone::class,
+        "Phone" => 'Varchar',
         "CountryCode" => DBCountry::class,
     ];
     private static $table_name = 'BaseModel';
