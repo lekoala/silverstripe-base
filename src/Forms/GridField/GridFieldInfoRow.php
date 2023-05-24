1a938c5f439f433b62bbd12cf0838c921da10b2f
Thomas <thomas@lekoala.be>
2023-05-24 09:32:41 +0200

    configurable margins

--- src/Forms/GridField/GridFieldInfoRow.php
+++ src/Forms/GridField/GridFieldInfoRow.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace LeKoala\Base\Forms\GridField;
 
 use SilverStripe\Forms\GridField\GridField_HTMLProvider;
@@ -17,6 +18,9 @@ class GridFieldInfoRow implements GridField_HTMLProvider
     protected $targetFragment;
     protected $content;
 
+    protected $marginBottom = "1rem";
+    protected $marginTop = "0";
+
     /**
      * @param string $targetFragment The HTML fragment to write the button into
      */
@@ -31,8 +35,48 @@ class GridFieldInfoRow implements GridField_HTMLProvider
      */
     public function getHTMLFragments($gridField)
     {
+        $mt = $this->marginTop;
+        $mb = $this->marginBottom;
         return array(
-            $this->targetFragment => '<div class="message" style="margin-bottom:10px;">' . $this->content . '</div>',
+            $this->targetFragment => '<div class="message" style="clear:both;margin-top:' . $mt . ';margin-bottom:' . $mb . ';">' . $this->content . '</div>',
         );
+    }
+
+    /**
+     * Get the value of marginBottom
+     */
+    public function getMarginBottom()
+    {
+        return $this->marginBottom;
+    }
+
+    /**
+     * Set the value of marginBottom
+     *
+     * @param string $marginBottom
+     */
+    public function setMarginBottom($marginBottom)
+    {
+        $this->marginBottom = $marginBottom;
+        return $this;
+    }
+
+    /**
+     * Get the value of marginTop
+     */
+    public function getMarginTop()
+    {
+        return $this->marginTop;
+    }
+
+    /**
+     * Set the value of marginTop
+     *
+     * @param string $marginTop
+     */
+    public function setMarginTop($marginTop)
+    {
+        $this->marginTop = $marginTop;
+        return $this;
     }
 }
