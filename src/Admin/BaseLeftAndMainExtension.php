7e4fe5903264d3d305ee7cd28c2f195d5185ad39
Thomas <thomas@lekoala.be>
2025-05-23 14:50:22 +0200

    tools for allowed hosts

--- src/Admin/BaseLeftAndMainExtension.php
+++ src/Admin/BaseLeftAndMainExtension.php
@@ -24,6 +24,8 @@ use LeKoala\Base\Theme\ThemeSiteConfigExtension;
 use SilverStripe\Forms\HTMLEditor\HTMLEditorConfig;
 use SilverStripe\Forms\HTMLEditor\TinyMCECombinedGenerator;
 use SilverStripe\Core\Extension;
+use SilverStripe\Control\Middleware\AllowedHostsMiddleware;
+use SilverStripe\Control\Director;
 
 /**
  * Available config
@@ -109,6 +111,27 @@ class BaseLeftAndMainExtension extends Extension
 
         Requirements::javascript("base/javascript/admin.js");
         $this->requireAdminStyles();
+
+        // Env
+        self::writeAllowedHosts();
+    }
+
+    /**
+     * You probably want to avoid a warning each time
+     * @link https://docs.silverstripe.org/en/5/developer_guides/security/secure_coding/#request-hostname-forgery
+     * @return void
+     */
+    public static function writeAllowedHosts(): void
+    {
+        $allowedHostsMiddleware = Injector::inst()->get(AllowedHostsMiddleware::class, true);
+        if ($allowedHostsMiddleware) {
+            if (empty($allowedHostsMiddleware->getAllowedHosts())) {
+                $env = Director::baseFolder() . '/.env';
+                if (is_file($env) && is_writable($env)) {
+                    file_put_contents($env, "\nSS_ALLOWED_HOSTS=\"*\"", FILE_APPEND);
+                }
+            }
+        }
     }
 
     public function ShowAVWarning(): bool
@@ -240,7 +263,7 @@ CSS;
      */
     protected function includeLastIcon()
     {
-        if (!class_exists(LeKoala\Admini\LeftAndMain::class)) {
+        if (!class_exists(\LeKoala\Admini\LeftAndMain::class)) {
             return;
         }
         $preconnect = <<<HTML
--- src/Subsite/SubsiteAdminExtension.php
+++ src/Subsite/SubsiteAdminExtension.php
@@ -1,7 +1,13 @@
 <?php
+
 namespace LeKoala\Base\Subsite;
 
 use SilverStripe\Core\Extension;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Control\Middleware\AllowedHostsMiddleware;
+use SilverStripe\Security\Permission;
+use SilverStripe\Control\Director;
+use SilverStripe\Subsites\Model\SubsiteDomain;
 
 /**
  * Class \LeKoala\Base\Subsite\SubsiteAdminExtension
@@ -18,5 +24,39 @@ class SubsiteAdminExtension extends Extension
     public function Backlink()
     {
         return 'admin/subsites';
+    }
+
+    public function init(): void
+    {
+        $allowedHostsMiddleware = Injector::inst()->get(AllowedHostsMiddleware::class, true);
+        if ($allowedHostsMiddleware) {
+            $force = isset($_GET['refresh_allowed_hosts']) && Permission::check('ADMIN');
+            if (empty($allowedHostsMiddleware->getAllowedHosts()) || $force) {
+                self::writeHostsListToEnv();
+            }
+        }
+    }
+
+    public static function writeHostsListToEnv(): void
+    {
+        $hosts = implode(',', self::generateHostsList());
+        $env = Director::baseFolder() . '/.env';
+        if (is_file($env) && is_writable($env)) {
+            file_put_contents($env, "\nSS_ALLOWED_HOSTS=\"$hosts\"", FILE_APPEND);
+        }
+    }
+
+    /**
+     * @link https://docs.silverstripe.org/en/5/developer_guides/security/secure_coding/#request-hostname-forgery
+     * @return array<string>
+     */
+    public static function generateHostsList(): array
+    {
+        $list = SubsiteDomain::get()->columnUnique('Domain');
+        $host = Director::host();
+        if (!in_array($host, $list)) {
+            $list[] = $host;
+        }
+        return $list;
     }
 }
